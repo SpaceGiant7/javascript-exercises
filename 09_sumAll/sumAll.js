4799be94391ca84f317b532a86ca55b6629c3fcd
Andrew Wagemakers <andrewwagemakers@gmail.com>
2025-05-11 11:53:17 -0600

    Finished lesson 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let output = 0;
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+    if (end < start) {
+        let temp = end;
+        end = start;
+        start = temp;
+    }
+    for (let i = start; i <= end; i++) {
+        output += i;
+    }
+    return output;
 };
 
 // Do not edit below this line
