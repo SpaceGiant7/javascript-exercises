0d7fcf5fb6c59c953244e8f245c2be7def893401
Andrew Wagemakers <andrewwagemakers@gmail.com>
2025-05-11 15:21:40 -0600

    Finish lesson 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const roundToOne = function(number) {
+    return Math.round(number * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(input) {
+    return roundToOne((input - 32) * 5 / 9);
+};
+
+const convertToFahrenheit = function(input) {
+    return roundToOne(input * 9 / 5 + 32);
 };
 
 // Do not edit below this line
