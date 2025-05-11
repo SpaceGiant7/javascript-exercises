6d8793becf54d4fd3294640eccc5f3f18f6c3ffe
Andrew Wagemakers <andrewwagemakers@gmail.com>
2025-05-11 11:18:25 -0600

    Finished lesson 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let output = "";
+    for (const c of input) {
+        output = c + output;
+    }
+    return output;
 };
 
 // Do not edit below this line
