df294538df5f0fa399b8ff37ac7250069a2891d1
Andrew Wagemakers <andrewwagemakers@gmail.com>
2025-05-11 11:45:25 -0600

    finished lesson 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let output = [];
+    for (const a of arr) {
+        let isHit = false;
+        for (const b of args) {
+            if (a === b) {
+                isHit = true;
+            }
+        }
+        if (!isHit) {
+            output.push(a);
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
