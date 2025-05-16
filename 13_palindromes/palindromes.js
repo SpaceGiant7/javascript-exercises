20cd809a65f534e6dfeef470bb901dfec02955f0
Andrew Wagemakers <andrewwagemakers@gmail.com>
2025-05-16 09:55:55 -0600

    finished lesson 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
+const isPunctuation = function(char) {
+    return "!,. ".includes(char)
+}
 
+const isNumber = function(char) {
+    let numberResult = parseInt(char)
+    return !isNaN(numberResult)
+}
+
+const palindromes = function (input) {
+    let result = "";
+    let modifiedInput = "";
+    for (const c of input) {
+        if (!isPunctuation(c)) {
+            if (!isNumber(c)) {
+                result = c.toLowerCase() + result;
+                modifiedInput = modifiedInput + c.toLowerCase();
+            } else {
+                result = c + result;
+                modifiedInput = modifiedInput + c;
+            }
+        }
+    }
+    console.log(result)
+    console.log(modifiedInput)
+    return result === modifiedInput;
 };
 
 // Do not edit below this line
