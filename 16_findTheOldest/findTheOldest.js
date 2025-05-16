64fc88a991e61cc51a00427a0c8d41d9374872a9
Andrew Wagemakers <andrewwagemakers@gmail.com>
2025-05-16 10:26:56 -0600

    finished lesson 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const currentYear = 2025;
 
+const getAge = function(person) {
+    let deathYear = currentYear;
+    if ("yearOfDeath" in person) {
+        deathYear = person.yearOfDeath
+    }
+    return deathYear - person.yearOfBirth;
+}
+
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let oldestIndex = 0;
+    for (let i = 0; i < people.length; i ++) {
+        let age = getAge(people[i])
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestIndex = i;
+        }
+    }
+    return people[oldestIndex];
 };
 
 // Do not edit below this line
