eb7c5928b140b618a45047c67670968da5771626
Andrew Wagemakers <andrewwagemakers@gmail.com>
2025-05-16 10:08:32 -0600

    Finished lesson 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS"
+    let lowest = 0;
+    let highest = 0;
+    for (let i = 0; i < n; i++) {
+        let temp = lowest + highest;
+        lowest = highest;
+        highest = temp;
+        if (i === 0) {
+            highest = 1;
+        }
+    }
+    return highest
 };
 
 // Do not edit below this line
