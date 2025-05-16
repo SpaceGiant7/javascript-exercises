52c5ab131cfebd13170218de82b625e997f478d9
Andrew Wagemakers <andrewwagemakers@gmail.com>
2025-05-16 09:15:56 -0600

    Finished lesson 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1+n2
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+    	return n1 - n2
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let result = 0;
+    for (const i of nums) {
+        result = result + parseInt(i);
+    }
+    return result;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+    let result = 1;
+    for (const i of nums) {
+        result = result * parseInt(i);
+    }
+    return result;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return multiply(Array(exponent).fill(base));
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result = result * i;
+    }
+    return result;
 };
 
 // Do not edit below this line
